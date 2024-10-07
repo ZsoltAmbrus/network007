34c54a753025a6786db939008b12d37378a302a4
Ambrus Zsolt <ambruszsolt@ktch.hu>
2024-10-07 23:37:58 +0200

    network

---
+++ src/App.java
@@ -0,0 +1,16 @@
+/*
+* File: App.java
+* Author: Ambrus Zsolt
+* Copyright: 2024, Ambrus Zsolt
+* Group: Szoft II/I/N
+* Date: 2024-10-06
+* Github: https://github.com/ZsoltAmbrus
+* Licenc: GNU GPL
+*/
+public class App {
+    public static void main(String[] args) throws Exception {
+        ConnectionManager manager = new ConnectionManager();
+        manager.establishConnection("Network");
+        manager.terminateConnection();
+    }
+}
