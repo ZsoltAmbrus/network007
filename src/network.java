1aab9f6555648a2487845638cc1dfbda9afe8336
Ambrus Zsolt <ambruszsolt@ktch.hu>
2024-10-07 23:39:20 +0200

    networkk

---
+++ src/network.java
@@ -0,0 +1,20 @@
+/*
+* File: network.java
+* Author: Ambrus Zsolt
+* Copyright: 2024, Ambrus Zsolt
+* Group: Szoft II/I/N
+* Date: 2024-10-06
+* Github: https://github.com/ZsoltAmbrus
+* Licenc: GNU GPL
+*/
+class ConnectionManager {
+    public void establishConnection(String networkName) {
+        System.out.println("Connecting to " + networkName + "...");
+        // Kapcsolódási logika
+    }
+
+    public void terminateConnection() {
+        System.out.println("Disconnecting...");
+        // Kapcsolat bontási logika
+    }
+}
